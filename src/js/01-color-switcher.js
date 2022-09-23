1edb7a86f7044c7fca625950deb45cdad46755f6
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-09-23 17:00:37 +0300

    task - 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.buttonStop.setAttribute('disabled', '');
+let timer = null;
+
+refs.buttonStart.addEventListener('click', () => {
+  timer = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    refs.buttonStart.setAttribute('disabled', '');
+    refs.buttonStop.removeAttribute('disabled');
+
+});
+
+refs.buttonStop.addEventListener('click', () => {
+  clearInterval(timer);
+    refs.buttonStart.removeAttribute('disabled');
+    refs.buttonStop.setAttribute('disabled', '');
+});
